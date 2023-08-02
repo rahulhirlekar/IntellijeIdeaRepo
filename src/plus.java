190fc86da38d93172907137c8fecfecbb9043ba5
RahulHirlekar <rahulhirlekar798@gmail.com>
2023-08-02 13:28:35 +0530

    intial version

---
+++ src/plus.java
@@ -0,0 +1,52 @@
+public class plus {
+
+    static int a = 10;
+    static int b = 15;
+    static int c = 20;
+    static int d = 25;
+
+    public static void sum() {
+      int   e = a + b;
+
+        System.out.println(" sum = " +  e);
+    }
+
+    public static void sub() {
+       int  f = b - c;
+        System.out.println("sub = " + f);
+
+    }
+
+        public static void mul() {
+
+           int g = c * d;
+            System.out.println("mul = " +  g);
+
+    }
+         public static void div () {
+
+             int h =  d/a;
+        System.out.print ("div = " + h ) ;
+         }
+         public static void  main(String[] args) {
+       sum ();
+        sub();
+        mul();
+         div () ;
+         }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
